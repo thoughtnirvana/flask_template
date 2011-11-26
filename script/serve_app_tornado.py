08bdc82dcfc532d406be847ecb77f7f019cbcc04
Rahul Kumar <rahulsinner@gmail.com>
2011-11-27 04:37:41 +0530

    Refactoring module to make it import friendly.

--- script/serve_app_tornado.py
+++ script/serve_app_tornado.py
@@ -1,10 +1,14 @@
-#!/usr/bin/env python
-from tornado.wsgi import WSGIContainer
-from tornado.httpserver import HTTPServer
-from tornado.ioloop import IOLoop
-import main
+def run_server(app, port=8080):
+    from tornado.wsgi import WSGIContainer
+    from tornado.httpserver import HTTPServer
+    from tornado.ioloop import IOLoop
+    import main
 
-# Initialize app and serve.
-http_server = HTTPServer(WSGIContainer(main.init()))
-http_server.listen(8080)
-IOLoop.instance().start()
+    # Initialize app and serve.
+    http_server = HTTPServer(WSGIContainer(main.init()))
+    http_server.listen(port)
+    IOLoop.instance().start()
+
+if __name__ == '__main__':
+    import main
+    run_server(main.init())
