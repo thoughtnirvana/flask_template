a16098bc1aeec530917e3d98d80ef24cab995cae
Rahul Kumar <rahulsinner@gmail.com>
2011-11-27 00:54:38 +0530

    Cleaned up route processing.

--- config/urls.py
+++ config/urls.py
@@ -6,63 +6,78 @@ from flask import Blueprint
 
 routes = [
     'app.views.hello',
-    ('/hello', 'hello', 'hello', {}),
+    ('/hello', 'hello'),
+    'app.views.pluggable',
+    ('/pluggable', ('Pluggable', dict(template_name='hello.html'))),
+    ('/restful', ('Restful', dict(template_name='hello.html'))),
 ]
 
-pluggable_views = [
-    'app.views.pluggable',
-    ('/pluggable', 'pluggable', ('Pluggable', dict(template_name='hello.html')), {}),
-    ('/restful', 'restful', ('Restful', dict(template_name='hello.html')), {}),
-]
 
 def set_urls(app):
     """
     Connects url patterns to actions for the given wsgi `app`.
     """
-    _set_routes(app)
-    _set_pluggable_views(app)
-
-def _set_routes(app):
-    """
-    Sets module urls.
-    """
+    module = None
+    blueprint = None
     for rule in routes:
         if isinstance(rule, str):
-            _set_routes.module = import_string(rule)
             # Register blueprint.
-            _register_blueprint(app, rule)
+            if blueprint:
+                if blueprint.name not in app.blueprints:
+                    app.register_blueprint(blueprint)
+            # New blueprint.
+            blueprint_path = rule
+            blueprint_name = blueprint_path.split('.')[-1]
+            blueprint = Blueprint(blueprint_name, blueprint_path,
+                                  static_folder='static',
+                                  template_folder='templates')
+            module = import_string(rule)
             continue
         # Set url rule.
-        url_rule, endpoint, view_func, opts = rule
-        view_func = getattr(_set_routes.module, view_func)
-        app.add_url_rule(url_rule, endpoint=endpoint,
-                         view_func=view_func, **opts)
-_set_routes.module = None
-
-def _set_pluggable_views(app):
-    """
-    Sets url rules for pluggable views.
-    """
-    for rule in pluggable_views:
-        if isinstance(rule, str):
-            _set_pluggable_views.module = import_string(rule)
-            # Register blueprint.
-            _register_blueprint(app, rule)
-            continue
-        # Set url rule.
-        url_rule, endpoint, (view_class, kwargs), opts = rule
-        view_func = getattr(_set_pluggable_views.module, view_class).as_view(endpoint, **kwargs)
-        app.add_url_rule(url_rule, view_func=view_func, **opts)
-_set_pluggable_views.module = None
-
-def _register_blueprint(app, blueprint_path,
-                        static_folder='static', template_folder='templates'):
-    """
-    Registers `blueprint` with the app.
-    """
-    blueprint_name = blueprint_path.split('.')[-1]
-    if blueprint_name not in app.blueprints:
-        blueprint = Blueprint(blueprint_name, blueprint_path,
-                              static_folder='static',
-                              template_folder='templates')
+        url_rule, endpoint, view_func, opts = _parse_rule(rule)
+        view_func = _get_view_func(module, view_func, endpoint)
+        blueprint.add_url_rule(url_rule, endpoint=endpoint,
+                               view_func=view_func, **opts)
+    # Register last blueprint.
+    if blueprint and blueprint.name not in app.blueprints:
         app.register_blueprint(blueprint)
+
+def _get_view_func(module, view_func, endpoint=None):
+    """
+    If `view_func` is a string, gets attribute `view_func` from `module`.
+    If `view_func` is a 2-tuple, constructs pluggable view and returns it.
+    """
+    if isinstance(view_func, str):
+        return getattr(module, view_func)
+    view_class, kwargs = view_func
+    view_class = getattr(module, view_class)
+    endpoint = endpoint or view_class.__name__.lower()
+    view_func = view_class.as_view(endpoint, **kwargs)
+    return view_func
+
+def _parse_rule(rule):
+    """
+    Breaks `rule` into `url`, `endpoint`, `view_func` and `opts`
+    """
+    length = len(rule)
+    if length == 4:
+        # No processing required.
+        return rule
+    elif length == 3:
+        rule = list(rule)
+        endpoint = None
+        opts = {}
+        if isinstance(rule[2], dict):
+            # Options passed.
+            opts = rule[2]
+            view_func = rule[1]
+        else:
+            # Endpoint passed.
+            endpoint = rule[1]
+            view_func = rule[2]
+        return (rule[0], endpoint, view_func, opts)
+    elif length == 2:
+        url_rule, view_func = rule
+        return (url_rule, None, view_func, {})
+    else:
+        raise ValueError('URL rule format not proper %s' % (rule, ))
