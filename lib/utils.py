d44205aa0337c1832094aac4b83ceda1da7e2115
Rahul Kumar <rahulsinner@gmail.com>
2011-11-27 00:54:52 +0530

    Simple check is enought for running pdb.

--- lib/utils.py
+++ lib/utils.py
@@ -2,20 +2,12 @@
 """
 Misc. utilities.
 """
-import sys
-
 
 def set_trace():
     """
     Wrapper for ``pdb.set_trace``.
-    Restore ``stdin``, ``stdout`` and ``stderr``.
     """
     from flask import current_app
     if not current_app.debug: return
-    try:
-        import ipdb as pdb
-    except ImportError:
-        import pdb
-    debugger = pdb.Pdb(stdin=sys.__stdin__,
-                       stdout=sys.__stderr__)
-    debugger.set_trace(sys._getframe().f_back)
+    import pdb
+    pdb.set_trace()
