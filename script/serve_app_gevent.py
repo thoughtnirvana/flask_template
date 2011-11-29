f3ad33a312dcfaaa78985fd5e67ba264355fe4ae
Rahul Kumar <rahulsinner@gmail.com>
2011-11-29 14:10:17 +0530

    Use pywsgi instead of wsgi.

--- script/serve_app_gevent.py
+++ script/serve_app_gevent.py
@@ -2,7 +2,7 @@ from gevent.monkey import patch_all
 patch_all()
 
 def run_server(app, port=8080):
-    from gevent.wsgi import WSGIServer
+    from gevent.pywsgi import WSGIServer
     http_server = WSGIServer(('', port), app)
     http_server.serve_forever()
 
