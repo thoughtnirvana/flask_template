29039f21f2d04280d826d7547e0673c2a4db185e
Rahul Kumar <rahulsinner@gmail.com>
2012-02-17 13:34:56 +0530

    Adding examples of validation for pluggable and restful views.

--- blueprints/app/views/pluggable.py
+++ blueprints/app/views/pluggable.py
@@ -1,12 +1,15 @@
 from flask import render_template
 from flask.views import View
 from flask.views import MethodView
+from lib.flask_augment import check_args
 
 class Pluggable(View):
     def __init__(self, template_name):
         self.template_name = template_name
 
     def dispatch_request(self):
+        errors = check_args(a=(lambda x: x and int(x) > 10, 'Should be greater than 10.'))
+        if errors: return str(errors)
         return render_template(self.template_name, name='pluggable')
 
 class Restful(MethodView):
@@ -14,7 +17,6 @@ class Restful(MethodView):
         self.template_name = template_name
 
     def get(self):
+        errors = check_args(a=(lambda x: x, 'Value must be provided.'))
+        if errors: return str(errors)
         return render_template(self.template_name, name='restful')
-
-    def _get_handler(self, errors):
-        return str(errors)
