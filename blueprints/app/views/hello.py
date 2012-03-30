a90b2077ce5ed76311744e1aa7f3438ce5e88632
Rahul Kumar <rahulsinner@gmail.com>
2012-03-30 15:36:25 +0530

    Example for slim templates.

--- blueprints/app/views/hello.py
+++ blueprints/app/views/hello.py
@@ -2,7 +2,7 @@ from flask import render_template
 from lib.flask_augment import ensure_args, ensure_presence
 
 def hello():
-    return render_template('hello.html')
+    return render_template('hello.slim')
 
 @ensure_presence(token=1)
 @ensure_args(a=(lambda x: x and (int(x) > 10), '"a" should be greater than 10'))
